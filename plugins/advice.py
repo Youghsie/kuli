fc9257f2130012394d39f6b26cfd7084951c4858
Itoshie Ryn. <randaka98@gmail.com>
2024-11-12 06:12:32 +0700

    Update advice.py

--- plugins/advice.py
+++ plugins/advice.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from TheApi import api
 
 from config import LOG_GROUP_ID
-from DanteMusic import app
+from RynMusic import app
 
 
 @app.on_message(filters.command("advice"))
